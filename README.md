e09b4c2b1d42d4feeabf4bc3eba7fbae713c2afb
Alexey Sheplyakov <asheplyakov@altlinux.org>
2020-12-01 12:13:41 +0400

    Works correctly if gzip magic is not at the multiple of 4 bytes

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ Some Linux distributions (in particular [ALT Linux](http://www.altlinux.org))
 use tricky initramfs images which consist of concatenated gzip archives.
 This little program helps to split such initramfs images into individual
 gzip files.
+
+## Usage
+
+```bash
+$ gzipsplit /path/to/full.cz
+  writing file #1
+  writing file #2
+  writing file #3
+```
+
+## Building
+
+```bash
+$ make check
+```
--- gzipsplit.c
+++ gzipsplit.c
@@ -19,38 +19,113 @@ void switch_file(FILE **current, int *count) {
 		exit(5);
 }
 
-
-int main(int argc, char **argv) {
-	char buf[4];
-	const char magic[3] = "\x1F\x8B\x08";
+static int can_be_flg(char c) {
 	const char flg_mask = 0x1f;
-	size_t nread;
+	if ((c & ~flg_mask) == 0)
+		return 1;
+	else
+		return 0;
+}
+
+static void process(FILE *in) {
 	int count = 0;
 	FILE* current = NULL;
+	int c1, c2, c3, c4;
+	size_t off = 0;
+	while (1) {
+		c1 = c2 = c3 = c4 = EOF;
+
+		c1 = fgetc(in);
+		if (c1 == EOF)
+			break;
+		off++;
+		if (!current) {
+			printf("writing file #1\n");
+			switch_file(&current, &count);
+			if (fputc(c1, current) == EOF)
+				exit(11);
+			continue;
+		}
+
+		if ((char)c1 == '\x1F') {
+			c2 = fgetc(in);
+			if (c2 == EOF) {
+				if (fputc(c1, current) == EOF)
+					exit(11);
+				break;
+			}
+			off++;
+			if ((char)c2 == '\x8B') {
+				c3 = getc(in);
+				if (c3 == EOF) {
+					if (fputc(c1, current) == EOF)
+						exit(11);
+					if (fputc(c2, current) == EOF)
+						exit(11);
+					break;
+				}
+				off++;
+				if ((char)c3 == '\x08') {
+					c4 = getc(in);
+					if (c4 == EOF) {
+						if (fputc(c1, current) == EOF)
+							exit(11);
+						if (fputc(c2, current) == EOF)
+							exit(11);
+						if (fputc(c3, current) == EOF)
+							exit(11);
+						break;
+					}
+					off++;
+					if (can_be_flg((char)c4)) {
+						printf("writing file #%d\n", count + 1);
+						switch_file(&current, &count);
+					}
+					if (fputc(c1, current) == EOF)
+						exit(11);
+					if (fputc(c2, current) == EOF)
+						exit(11);
+					if (fputc(c3, current) == EOF)
+						exit(11);
+					if (fputc(c4, current) == EOF)
+						exit(11);
+				} else {
+					/* c3 != '\x08' */
+					if (fputc(c1, current) == EOF)
+						exit(11);
+					if (fputc(c2, current) == EOF)
+						exit(11);
+					if (fputc(c3, current) == EOF)
+						exit(11);
+				}
+			} else {
+				/* c2 != '\x8B' */
+				if (fputc(c1, current) == EOF)
+					exit(11);
+				if (fputc(c2, current) == EOF)
+					exit(11);
+			}
+		} else {
+			/* c1 != '\x1F' */
+			if (fputc(c1, current) == EOF)
+				exit(11);
+		}
+	}
+	if (current) {
+		fflush(current);
+		fclose(current);
+		current = NULL;
+	}
+}
+
+int main(int argc, char **argv) {
 	FILE* in;
 	in = fopen(argv[1], "r");
 	if (!in) {
 		exit(7);
 	}
-
-	while (!feof(in)) {
-		nread = fread(buf, sizeof(buf), 1, in);
-		if (nread < 1) {
-			fwrite(buf, sizeof(buf), 1, current);
-			continue;
-		}
-		if (!current) {
-			switch_file(&current, &count);
-		} else if (strncmp(buf, magic, sizeof(magic)) == 0) {
-			if ((buf[3] & ~flg_mask) == 0) {
-				printf("switching file %d\n", count);
-				switch_file(&current, &count);
-			}
-		}
-		fwrite(buf, sizeof(buf), 1, current);
-	}
-	fflush(current);
-	fclose(current);
+	process(in);
+	fclose(in);
 	return 0;
 }
 
